aa2d282aec9913e15311772bf34600245a084f29
ufofon2 <ufofon@gmail.com>
2016-09-05 11:22:24 +0900

    Initial commit

---
+++ app/src/main/java/a816/android/soldesk/mbc_1/Mbc_1Activity.java
@@ -0,0 +1,13 @@
+package a816.android.soldesk.mbc_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mbc_1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mbc_1);
+    }
+}
